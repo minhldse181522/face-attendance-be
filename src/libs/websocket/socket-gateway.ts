593a264c73de84221dfe4c39492384beb3ab17d8
minhle <minhldse181522@fpt.edu.vn>
2025-08-02 13:12:29 +0700

    add socket gateway

---
+++ src/libs/websocket/socket-gateway.ts
@@ -0,0 +1,26 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: false, // nếu dùng cookie thì true
+  },
+})
+export class SocketGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log('Client connected:', client.id);
+  }
+
+  sendToAll(message: string) {
+    this.server.emit('message', message);
+  }
+}
